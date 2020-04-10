38e9dd14b36b3b9578d76259a6cc5d8fe95a88c0
Sergey Poznyak <Sergeeeekg@gmail.com>
2020-04-10 08:14:00 +0300

    Implemented react-router module

--- packages/react-router/src/index.tsx
+++ packages/react-router/src/index.tsx
@@ -1,14 +1,47 @@
-import {Route} from 'react-router';
-import {createModule} from '@ts-module-system/core';
+import * as React from 'react';
+import { createModule } from '@ts-module-system/core';
+import {BrowserRouter, HashRouter, RouteProps, MemoryRouter, Switch} from 'react-router-dom';
+
+type RouterType = 'memory' | 'browser' | 'hash';
 
 interface ReactRouterModuleDeps {
-  routes: readonly (typeof Route)[],
+  initialEntries?: History.LocationDescriptor[],
+  initialIndex?: number,
+  routes: readonly React.ReactElement<RouteProps>[],
+  Child?: React.ComponentType<{children?: React.ReactNode}>,
+  type?: RouterType,
 }
 
-// export const ReactRouterModule = createModule({
-//   start({routes}: ReactRouterModuleDeps) {
-//     return {
-//       instance:
-//     };
-//   }
-// });
+type CommonRouterProps = React.ComponentProps<typeof MemoryRouter> & React.ComponentProps<typeof BrowserRouter> & React.ComponentProps<typeof HashRouter>;
+const routerTypeToModule: {
+  [K in RouterType]: React.ComponentType<CommonRouterProps>
+} = {
+  memory: MemoryRouter,
+  browser: BrowserRouter,
+  hash: HashRouter,
+}
+
+export function ReactRouterModule({
+    initialEntries,
+    initialIndex,
+    routes,
+    Child = ({children}) => <>{children}</>,
+    type = 'browser'
+  }: ReactRouterModuleDeps) {
+
+  const Router = routerTypeToModule[type];
+
+  function ReactRouter() {
+    return (
+      <Router initialEntries={initialEntries} initialIndex={initialIndex}>
+        <Child>
+          <Switch>
+            {routes}
+          </Switch>
+        </Child>
+      </Router>
+    );
+  }
+
+  return createModule(ReactRouter);
+};
